b5ee298c68168e98a008d0fe1b98bef464f9667e
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-11-26 12:45:58 +0100

    wip: lobby: live updates with websockets

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi import WebSocket
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
 # In-memory storage for game players (game_id -> set of player names)
 game_players = {}
+
+# WebSocket connections (game_id -> set of WebSocket connections)
+game_connections = {}
 
 
 @app.get("/game/{game_id}", response_class=HTMLResponse)
@@ -26,6 +30,9 @@ async def join_game(request: Request, game_id: str, player_name: str = Form(...)
     if game_id not in game_players:
         game_players[game_id] = set()
     game_players[game_id].add(player_name)
+
+    # Broadcast updated player list
+    await broadcast_player_list(game_id)
 
     # Get other players (excluding current player)
     other_players = game_players[game_id] - {player_name}
@@ -47,8 +54,61 @@ async def leave_game(request: Request, game_id: str, player_name: str = Form(...
     if game_id in game_players:
         game_players[game_id].discard(player_name)
 
+    # Broadcast updated player list
+    await broadcast_player_list(game_id)
+
     # Return the input form
     return templates.TemplateResponse(
         "game_leave.html",
         {"request": request, "game_id": game_id},
     )
+
+
+@app.websocket("/ws/game/{game_id}")
+async def game_websocket(websocket: WebSocket, game_id: str):
+    await websocket.accept()
+
+    # Add connection to the game
+    if game_id not in game_connections:
+        game_connections[game_id] = set()
+    game_connections[game_id].add(websocket)
+
+    try:
+        # Send initial player list
+        current_players = list(game_players.get(game_id, set()))
+        await websocket.send_json(
+            {"type": "players_update", "players": current_players}
+        )
+
+        # Keep connection alive
+        while True:
+            # Wait for any message (we don't expect client messages for now)
+            await websocket.receive_text()
+    except Exception:
+        # Remove connection on disconnect
+        if game_id in game_connections:
+            game_connections[game_id].discard(websocket)
+            if not game_connections[game_id]:
+                del game_connections[game_id]
+
+
+async def broadcast_player_list(game_id: str):
+    """Broadcast the current player list to all connected clients for a game."""
+    if game_id not in game_connections:
+        return
+
+    current_players = list(game_players.get(game_id, set()))
+    message = {"type": "players_update", "players": current_players}
+
+    # Send to all connected clients
+    disconnected = set()
+    for connection in game_connections[game_id]:
+        try:
+            await connection.send_json(message)
+        except Exception:
+            disconnected.add(connection)
+
+    # Remove disconnected clients
+    game_connections[game_id] -= disconnected
+    if not game_connections[game_id]:
+        del game_connections[game_id]
