6de062944798136a55063b135fe9451ea73be879
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-11-26 12:08:30 +0100

    wip: lobby: enter name

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -6,13 +6,16 @@ app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
 
-@app.get("/items/{id}")
-async def read_item(_: Request, id: str):
-    return {"item": id}
-
-
 @app.get("/game/{game_id}", response_class=HTMLResponse)
 async def game_lobby(request: Request, game_id: str):
     return templates.TemplateResponse(
         "game_lobby.html", {"request": request, "game_id": game_id}
+    )
+
+
+@app.post("/game/{game_id}/join", response_class=HTMLResponse)
+async def join_game(request: Request, game_id: str, player_name: str = Form(...)):
+    return templates.TemplateResponse(
+        "game_joined.html",
+        {"request": request, "game_id": game_id, "player_name": player_name},
     )
